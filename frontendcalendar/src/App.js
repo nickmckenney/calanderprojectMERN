2b22d47277f8e4eb49073d2df7360df344892c66
NickMckenney <mckenney2001@gmail.com>
2019-11-18 14:39:37 -0500

    added new componenet for aside

--- frontendcalendar/src/App.js
+++ frontendcalendar/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Day from "./Container";
+import Aside from "./Aside";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Day />
+        <Aside />
       </div>
     );
   }
---
+++ frontendcalendar/src/Aside.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+class Aside extends Component {
+  render() {
+    return (
+      <div>
+        <h1>q</h1>
+      </div>
+    );
+  }
+}
+export default Aside;
