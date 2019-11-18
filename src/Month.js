b3d839bb6cb758827feb127ddbae7dd8aacdb2c6
NickMckenney <mckenney2001@gmail.com>
2019-11-18 17:15:43 -0500

    Added counter for months

--- src/Month.js
+++ src/Month.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import "./App.css";
+
 class Month extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      months: ["January", "Feburay", "March"]
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      index: 0
     };
   }
-
+  clickHandler() {
+    if (this.state.index < 11) {
+      console.log(this.state.index);
+      this.setState({
+        index: this.state.index + 1
+      });
+    }
+  }
   render() {
     return (
       <div className="month">
-        <h1>{this.state.months[1]}</h1>
+        <button className="btn" onClick={() => this.clickHandler()}>
+          Click Me
+        </button>
+
+        <h1>{this.state.months[this.state.index]}</h1>
       </div>
     );
   }
