2f77c3cd1df89952c213e05db4e76877aa998d3a
NickMckenney <mckenney2001@gmail.com>
2019-11-18 14:53:38 -0500

    added unique listners to each div for cal

--- frontendcalendar/src/Aside.js
+++ frontendcalendar/src/Aside.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 class Aside extends Component {
   render() {
-    return (
-      <div>
-        <h1>q</h1>
-      </div>
-    );
+    return <div className="aside"></div>;
   }
 }
 export default Aside;
--- frontendcalendar/src/Container.js
+++ frontendcalendar/src/Container.js
@@ -5,15 +5,13 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: []
     };
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    this.setState({
-      squares: squares
-    });
+  handleClick(event) {
+    console.log(event);
+    this.setState({});
   }
 
   renderSquare(i) {
@@ -29,49 +27,49 @@ class Board extends React.Component {
     return (
       <div className="container">
         <div className="board-row">
+          {this.renderSquare(1)}
           {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
         </div>
         <div className="board-row">
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
         </div>
         <div className="board-row">
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+          {this.renderSquare(20)}
+          {this.renderSquare(21)}
         </div>
         <div className="board-row">
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+          {this.renderSquare(25)}
+          {this.renderSquare(26)}
+          {this.renderSquare(27)}
         </div>
         <div className="board-row">
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
-          {this.renderSquare(2)}
+          {this.renderSquare(28)}
+          {this.renderSquare(29)}
+          {this.renderSquare(30)}
+          {this.renderSquare(31)}
+          {this.renderSquare(32)}
+          {this.renderSquare(33)}
+          {this.renderSquare(34)}
         </div>
       </div>
     );
