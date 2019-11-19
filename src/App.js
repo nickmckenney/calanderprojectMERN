6519245f58dc1e7830f03d5d7fa217476969ae27
William Chrapcynski <wchrapcynski@gmail.com>
2019-11-19 16:11:24 -0500

    Created function to calculate day of week month starts

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class App extends Component {
       year: 2019,
       isLoading: false,
       holidayArray: [],
-      birthdayArray: []
+      birthdayArray: [],
+      dayOfTheWeek: 0
     };
     this.apiURL = "https://calendar-api-deploy.herokuapp.com/";
     this.months = [
@@ -34,6 +35,8 @@ class App extends Component {
     ];
     this.daysPerMonth = [null, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     this.monthStrings = [null, "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    this.monthCodes = [null, 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
+    this.centuryCode = 6;
   }
 
   grabHolidayMonth = () => {
@@ -56,6 +59,20 @@ class App extends Component {
         console.log("We've got a problem, sir.", err);
       });
   };
+
+  firstDay = () => {
+    let twoDigit = this.state.year % 100;
+    let yearCode = ((twoDigit + Math.floor(twoDigit / 4)) % 7)
+    let monthCode = this.monthCodes[this.state.month]
+    let leapYear = 0;
+    if (this.state.year % 4 === 0) {
+      leapYear = 1;
+    } else {
+      leapYear = 0;
+    }
+    let dayOfWeek = ((yearCode + monthCode + this.centuryCode + 1 + leapYear) %7 ) % 7;
+    this.state({ dayOfTheWeek: dayOfWeek });
+  }
 
   monthNext = () => {
     if (this.state.month === 12) {
@@ -86,6 +103,7 @@ class App extends Component {
   }
 
   render() {
+    this.firstDay()
     return (
       <div className="App">
         <Container {...this.state} dayCounter={this.dayCounter} />
