e252f2deaa9dbb13538875c3104436eac33fd394
Steven Kim <sskim14@gmail.com>
2019-11-21 09:28:50 -0500

    Implemented holiday data into new layout

--- src/Day.js
+++ src/Day.js
@@ -8,7 +8,15 @@ class Square extends React.Component {
 
   render() {
     if (this.props.value !== ".") {
-      return <div className={"square"}>{this.props.value}</div>;
+      return (
+        <div className={"square"}>{this.props.value}
+          <div className="holidayContainer">
+            <h4 className="info">{this.props.info}</h4>
+            <br></br>
+            <h4 className="holiday">{this.props.bDay}</h4>
+          </div>
+        </div>
+      )
     } else if (this.props.value === ".") {
       return <div className={"square fill"}>.</div>;
     }
